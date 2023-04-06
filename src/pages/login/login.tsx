19f55370a8bddf56a4c038c29a095e609b0a47be
WebToLaure <lmarmus@hotmail.fr>
2023-04-06 10:32:49 +0200

    fix: modif

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
+
 import "././login.css";
 
 
-export default function Login(props:any) {
+export default function Login(props: any) {
     return (
 
         <div className="login">
