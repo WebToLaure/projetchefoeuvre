d5ab04c54e92511c2fa6b547b6d8bfb7ad0c95eb
WebToLaure <lmarmus@hotmail.fr>
2023-04-06 10:30:53 +0200

    feat: navbar.tsx et css

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,15 +1,13 @@
-
 import './navbar.css';
-import HomePage from '../../pages/homePage/home';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
-export default function NavBar(props: any) {
+export default function NavBar(props: any): JSX.Element {
 
 
     return (
-       
+
         <nav className='navbar navbar-expand-md  bg-light sticky-top '>
-            <div className='container vw-100 px-0 h-100'>
+            <div className='container vw-100  h-100'>
                 <div className='navbar-brand'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
                         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -33,42 +31,41 @@ export default function NavBar(props: any) {
                     <div className='navBar_center'>
                         <ul className="navbar-nav ">
                             <li className="nav-item ">
-                                <Link to ="/" className="nav-link" >HOME</Link>
+                                <NavLink to="/" className="nav-link">HOME</NavLink>
                             </li>
                             <li className="nav-item ">
-                                <Link to ="/" className="nav-link">ABOUT</Link>
+                                <NavLink to="/contact" className="nav-link">CONTACT</NavLink>
                             </li>
                             <li className="nav-item ">
-                                <Link to ="/" className="nav-link">CONTACT</Link>
+                                <NavLink to="/write" className="nav-link" >WRITE</NavLink>
                             </li>
                             <li className="nav-item ">
-                                <Link to ="/" className="nav-link">WRITE</Link>
-                            </li>
-                            <li className="nav-item ">
-                                <Link to ="/" className="nav-link">LOGOUT</Link>
+                                <NavLink to="/logout" className="nav-link">LOGOUT</NavLink>
                             </li>
                         </ul>
                     </div>
                     <div className='navBar_right'>
-                        <img className='profilImage' src="/photos/photoProfil.jpg" alt="" />
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="searchIcon" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                        </svg>
-                        <ul className="navbar-nav navBarLogin">
-                            <li className="nav-item ">
-                                <a href="#" className="nav-link">Login</a>
-                            </li>
-                            <li className="nav-item navBarRegister">
-                                <a href="#" className="nav-link">Register</a>
-                            </li>
-                        </ul>
-                        
-                    </div>
+                        <NavLink to="/settings">< img className='profilImage' src="/photos/photoProfil.jpg" alt="" /></NavLink>
+
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="searchIcon" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                    <ul className="navbar-nav navBarLogin">
+                        <li className="nav-item ">
+                        <NavLink to="/users/login" className="nav-link">Login</NavLink>
+                        </li>
+                        <li className="nav-item navBarRegister">
+                        <NavLink to="/users/register" className="nav-link">Register</NavLink>
+                        </li>
+                    </ul>
+
                 </div>
             </div>
+        </div>
         </nav >
-      
-       
+
+
 
     )
 }
