a46ab11d4eb6569ba0dbfc4f41e8c75a1396981a
WebToLaure <lmarmus@hotmail.fr>
2023-04-06 10:31:12 +0200

    feat: continentsCard.tsx et css

---
+++ src/pages/ContinentsCards/continentsCards.tsx
@@ -0,0 +1,15 @@
+ import FlippableCard from '../../components/FlippableCard/flippableCard';
+
+
+
+
+export default function ContinentsCards() {
+
+    return(
+
+        <div className="container h-100 w-100">
+            <FlippableCard/>
+            
+        </div>
+    )
+} 
