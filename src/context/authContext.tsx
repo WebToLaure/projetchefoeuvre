bb78e49be055ca2e0b10e4aeed3f110aecf45728
WebToLaure <lmarmus@hotmail.fr>
2023-04-09 18:25:09 +0200

    feat:authContext

---
+++ src/context/authContext.tsx
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+
+export type Tuser = {
+    user: {
+        id: number,
+        gender:string,
+        pseudo: string,
+        email: string,   
+        photo: string,
+        createdAt:Date,
+        updatedAt:Date,
+        role: string,
+        topics: { id: number, continent: string, title: string, destinations: string, content: string, createdAt: Date, updatedAt: Date, deletedAt: Date }[],
+        commentaries: { id: number, content: string, createdAt: Date, updatedAt: Date, deletedAt: Date }[],
+    },
+    access_token: string
+}
+
+
+export interface IAuthContext {
+    user: Tuser | null;
+    setUser: (user:Tuser|null) => void;
+}
+
+// Pour créer du contexte, vous devez importer createContext et l’initialiser :
+export const AuthContext = createContext<IAuthContext>({
+    user: null,
+    setUser: () => { },
+
+})
