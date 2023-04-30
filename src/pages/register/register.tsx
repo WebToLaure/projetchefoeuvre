e0e0935e131e2b35651d003da00170507bcb8bdb
WebToLaure <lmarmus@hotmail.fr>
2023-04-30 13:35:37 +0200

    feat : register

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,7 +1,8 @@
 
-import { NavLink } from "react-router-dom";
+import { NavLink, Navigate, useNavigate } from "react-router-dom";
 import "./register.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/authContext";
 
 
 
@@ -33,8 +34,8 @@ export default function Register(props: any) {
     const [emailInput, setEmailInput] = useState("")
     const [passwordInput, setPasswordInput] = useState("")
     const [passwordConfirmInput, setPasswordConfirmInput] = useState("")
-
-
+    const auth = useContext(AuthContext)
+    const navigate = useNavigate();
     /**
      * @function Register
      * 
@@ -43,41 +44,48 @@ export default function Register(props: any) {
      * * Création du body register afin de les lier avec les input dans le return
      * * Faire appel aux requêtes back-end pour la relation Front/Back
      */
-    async function DataRegister(event: { preventDefault: () => void; }) {
+    async function Register(event: { preventDefault: () => void; }) {
 
         event.preventDefault()
-
-        // body du register sur la partie html
-        const body: ProfilRegister = {
-            gender: genderInput,
-            pseudo: pseudoInput,
-            email: emailInput,
-            password: passwordInput,
-            password_confirm: passwordConfirmInput,
-
-        }
 
         // Options de requêtes et envoi des données des input en BDD
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(body)
+            body: JSON.stringify({
+
+                gender: genderInput,
+                pseudo: pseudoInput,
+                email: emailInput,
+                password: passwordInput,
+                password_confirm: passwordConfirmInput,
+            })
         };
 
         const response = await fetch('http://localhost:8000/users/register', requestOptions);
         const responseJson = await response.json();
+        console.log(response);
+        console.log(responseJson);
 
-
-        //si nous avons la réponse json du register dans la console, alors nous faisons un reset des input du formulaire
+        //après validation de la requête, annuler toutes les saisies des inputs
 
         if (responseJson.statusCode === 201) {
+            console.log("test");
+            navigate("/auth/login")
             resetInput()
-            alert("Votre Compte a été créé avec succès");
-        }
-
-        else {
+            alert("Compte créé avec succès,vous pouvez vous connecter à présent")
+        } else if (responseJson === 400) {
+            alert("Votre civilité n'a pas été renseignée");
+        } else if (responseJson.statusCode === 404) {
+            alert("Votre mot de passe ne correspond pas au critère de sécurité");
+        } else if (responseJson.statusCode === 409) {
+            alert("Saisie incorrecte du mot de passe de confirmation");
+        } else if (responseJson.message === "L'Email existe déjà") {
+            alert(responseJson.message);
+        } else {
             return
         }
+
 
     }
 
@@ -89,45 +97,61 @@ export default function Register(props: any) {
         setPasswordInput("")
         setPasswordConfirmInput("")
 
-
-
-
-
-
-
     }
 
     return (
 
-        <div className="register">
-            <span className="registerTitle">Register</span>
+        <div className="container-fluid">
+            <div className="row justify-content-center register mx-2 ">
+                <div className="col-xl-4 col-md-8">
+                    <div className=" form-log rounded-5 shadow-5-strong p-3 ">
+                        <div className="col text-center text-white align-items-center  mb-3 mt-2">
+                            <h4 className="titleRegister m-0">Register</h4>
+                        </div>
 
-            <form className="registerForm mt-5">
-                <label htmlFor="gender" className="form-label">Gender</label>
-                <select name="gender" id="gender" className="form-select-sm registerInput" value={genderInput} onChange={(event) => setGenderInput(event.target.value)}>
-                    <option value="Miss">Miss</option>
-                    <option value="Mr">Mr</option>
-                    <option value="Other">...</option>
-                </select>
+                        <label htmlFor="gender" className="form-label text-white m-0">Gender</label>
+                        <div className="form-outline mt-2">
+                            <select name="gender" id="gender" className="form-select-sm registerInput col-6" value={genderInput} onChange={(event) => setGenderInput(event.target.value)}>
+                                <option value="Miss"></option>
+                                <option value="Miss">Miss</option>
+                                <option value="Mr">Mr</option>
+                                <option value="Other">...</option>
+                            </select>
+                        </div>
 
-                <label htmlFor="username" className="form-label">Pseudo</label>
-                <input type="text" name="username" id="username" className="form-control registerInput" placeholder="Enter your username.." value={pseudoInput} onChange={(event) => setPseudoInput(event.target.value)} />
+                        <label htmlFor="pseudo" className="form-label text-white m-0">Pseudo</label>
+                        <div className="form-outline mb-3 mt-2">
+                            <input type="text" id="pseudo" className="form-control registerInput" placeholder="Enter your pseudo.." value={pseudoInput} onChange={(event) => setPseudoInput(event.target.value)} />
+                        </div>
+                        <label htmlFor="email" className="form-label text-white m-0">Email address</label>
+                        <div className="form-outline mb-3 mt-2">
+                            <input type="email" id="email" className="form-control registerInput" placeholder="Enter your email.." value={emailInput} onChange={(event) => setEmailInput(event.target.value)} />
+                        </div>
+                        <label htmlFor="password" className="form-label text-white m-0">Password</label>
+                        <div className="form-outline mb-3 mt-2">
+                            <input type="password" className="form-control registerInput" id="password" placeholder="Enter your password.." value={passwordInput} onChange={(event) => setPasswordInput(event.target.value)} />
+                            <div id="passwordHelpBlock" className="form-text text-light text-bold">
+                                Your password must be 8-20 characters long, contain at least an upper case letter, a number, and must contain special characters..
+                            </div>
+                        </div>
+                        <label htmlFor="passwordConfirm" className="form-label text-white m-0">Password Confirm</label>
+                        <div className="form-outline mb-3 mt-2">
+                            <input type="password" className="form-control registerInput" id="passwordConfirm" placeholder="Confirm your password.." value={passwordConfirmInput} onChange={(event) => setPasswordConfirmInput(event.target.value)} />
+                            <div id="passwordHelpBlock" className="form-text text-light text-bold">
+                                Please confirm your password..
+                            </div>
+                        </div>
+                        <div className="col-center text-center align-items-center mt-3">
+                            <button type="button" className="btn-block registerButton col-4" onClick={Register}>Register</button>
+                        </div>
+                        <div className="col-center text-center justify-content-center align-items-center mt-2 mb-2">
+                            <NavLink to="/auth/login"><button className="LoginBtn btn-block rounded col-6 ">Se connecter</button></NavLink>
+                        </div>
 
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" name="email" id="email" className="form-control registerInput" placeholder="Enter your email.." value={emailInput} onChange={(event) => setEmailInput(event.target.value)} />
-
-                <label htmlFor="password" className="form-label">password</label>
-                <input type="password" name="password" id="password" className="form-control registerInput" placeholder="Enter your password.." value={passwordInput} onChange={(event) => setPasswordInput(event.target.value)} />
-
-                <label htmlFor="password" className="form-label">password Confirm</label>
-                <input type="password" name="password" id="password" className="form-control registerInput" placeholder="Enter your password.." value={passwordConfirmInput} onChange={(event) => setPasswordConfirmInput(event.target.value)} />
-                
-                <button className="registerButton" onSubmit={DataRegister}>Register</button>
-            </form>
-
-            <NavLink to="/users/login"><button className="registerLoginButton">LogIn</button></NavLink>
-
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
-// quand l'utilisateur se trouve sur le composant register , dans topBar, il faut que les boutons register,login,LOGOUT,write et Account disparaissent disparaissent
