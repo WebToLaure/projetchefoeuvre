5318574bba6360f785457b5d11311bd3bea7a9b6
WebToLaure <lmarmus@hotmail.fr>
2023-04-30 13:34:17 +0200

    feat : login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,8 @@
-
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "././login.css";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
+
 
 type ProfilLog = {
     email: string;
@@ -15,17 +15,18 @@ export default function Login(props: any) {
 
     const auth = useContext(AuthContext)
 
+    const navigate = useNavigate();
     async function fetchDataLog() {
-
-        const body: ProfilLog = {
-            email: emailInput,
-            password: passwordInput,
-        }
 
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(body)
+
+            body: JSON.stringify({
+
+                email: emailInput,
+                password: passwordInput,
+            })
         };
 
         const response = await fetch('http://localhost:8000/auth/login', requestOptions);
@@ -35,11 +36,17 @@ export default function Login(props: any) {
         if (responseJson.access_token) {
             auth.setUser({ ...responseJson });
             resetInputLog()
+            navigate("/");
         }
 
         else {
-            return
-        }
+            resetInputLog()
+            return (
+
+                alert("Email ou mot de passe incorrect")
+            )
+
+        };
 
     }
 
@@ -53,22 +60,54 @@ export default function Login(props: any) {
 
     return (
 
-        
-            <div className="login">
+        <div className="container-fluid ">
+            <div className="row justify-content-center mx-2 login ">
+                <div className="col-xl-4 col-md-8">
+                    <form className="container-form">
+                        <div className=" form-log rounded-5 shadow-5-strong p-5">
 
-                <span className="loginTitle">Se connecter</span>
-                <form className="loginForm">
-                    <label >Email</label>
-                    <input type="text" className="loginInput" placeholder="Enter your email.." value={emailInput} onChange={(event) => setEmailLogInput(event.target.value)} />
+                            {/* <!-- title connexion --> */}
+                            <div className="col text-center text-white align-items-center mb-5 mt-5">
+                                <h4>Connexion</h4>
+                            </div>
 
-                    <label >Password</label>
-                    <input type="password" className="loginInput" placeholder="Enter your password.." value={passwordInput} onChange={(event) => setPasswordLogInput(event.target.value)} />
-                    <NavLink to="/"> <button className="loginButton" onClick={fetchDataLog}>Se connecter</button></NavLink>
-                </form>
+                            {/* <!-- Email input --> */}
+                            <label htmlFor="email" className="text-white">Email</label>
+                            <div className="form-outline mb-3 mt-2">
+                                <input type="email" className="form-control" placeholder="Email" value={emailInput} onChange={(event) => setEmailLogInput(event.target.value)}></input>
+                            </div>
 
-                <NavLink to="/users/register"><button className="loginRegisterButton">Register </button></NavLink>
+                            {/* <!-- Password input --> */}
+                            <label htmlFor="password" className="text-white">Password</label>
+                            <div className="form-outline mb-3 mt-2">
+                                <input type="password" className="form-control" placeholder="Enter your password.." value={passwordInput} onChange={(event) => setPasswordLogInput(event.target.value)}></input>
+                            </div>
 
+                            {/* <!-- Simple link --> */}
+                            <div className="col-center text-center align-items-center">
+                                <a href="#!" className="link-light text-primary">Mot de passe oublié ?</a>
+                            </div>
+
+                            {/* <!-- Submit button Login--> */}
+                            <div className="col-center text-center align-items-center mt-4">
+                                <button type="button" className="btn-block loginButton rounded col-6" onClick={fetchDataLog}>Se connecter</button>
+                            </div>
+
+                            {/* <!-- Submit button Register --> */}
+                            <div className="col-center text-center align-items-center mt-5">
+                                <NavLink to="/users/register"><button type="button" className="btn btn-dark btn-block col-6">Créez votre compte</button></NavLink>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-      
+        </div>
+
     )
 }
+
+
+
+{/*}  <NavLink to="/users/register"><button className="loginRegisterButton mt-3 me-3 rounded border border-info">Register </button></NavLink>
+                        <NavLink to="/"><button className="d-flex text-decoration-none loginButton text-center border border-warning rounded  m-auto  mt-3" onClick={fetchDataLog}>Se connecter</button></NavLink>
+</div>*/}
