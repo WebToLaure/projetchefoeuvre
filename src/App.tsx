ac51d891d1b8e8cb5f6c59c9a617bf4352b9d0f4
WebToLaure <lmarmus@hotmail.fr>
2023-04-30 13:30:57 +0200

    feat: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ImageUploading from 'react-images-uploading';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
@@ -9,36 +10,53 @@ import Settings from './pages/settings/settings';
 import Write from './pages/write/write';
 import Login from './pages/login/login';
 import Register from './pages/register/register';
-import Topics from './pages/topics/topics';
 import Topic from './pages/topic/topic';
 import { PageNotFound } from './pages/PageNotFound/PageNotFound';
-import ContinentsCards from './pages/ContinentsCards/continentsCards';
+import ContinentsCards from './components/ContinentsCards/continentsCards';
 import { AuthContext, Tuser } from './context/authContext';
+import TopicsCards from './components/topicsCards/topicsCards';
+import Contact from './pages/contact/contact';
 
 
 function App() {
-  
   const [user, setUser] = useState<Tuser | null>(null);
-  
+  const [images, setImages] = React.useState([]);
+
+  const maxNumber = 69;
+
+  /*  const onChange = (imageList, addUpdateIndex) => {
+  */
+  // data for submit
+
+  /* console.log(imageList, addUpdateIndex);
+
+  setImages(imageList);
+}; */
 
   return (
     <AuthContext.Provider value={{
       user: user,
       setUser: setUser
     }}>
-    <div className='.container-sm px-2'>
-      <NavBar />
-      <Routes>
+      {/* {user === null && <Login></Login>}
+      {user !== null && <HomePage></HomePage>}  */}
+
+      <>
+        <NavBar />
+        <Routes>
           <Route path='/' element={<HomePage />} />{/* App sera le parent des routes posts et on trouvera la navbar fixe*/}
           <Route path="write" element={<Write />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/settings" element={<Settings />} />
-          <Route path="/users/login" element={<Login />} />
+          <Route path="/auth/login" element={<Login />} />
           <Route path="/users/register" element={<Register />} />
-          <Route path="/topics" element={<Topics />} /> {/*  permettra de consulter l'ensemble des posts */}
+          <Route path="/continents" element={<ContinentsCards />} /> {/*  permettra de consulter l'ensemble des posts */}
+          <Route path="/topicsCards/:id" element={<TopicsCards />} />
+          <Route path="/topic/:id" element={<Topic />} />
           <Route path="topics/delete/:id" element={<Topic />} /> {/* permettra de supprimer un post  */}
           <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </div>
+        </Routes>
+      </>
     </AuthContext.Provider>
   );
 }
