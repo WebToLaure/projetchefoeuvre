e4ef2f0c4cb3ba3b7ca2e14e5482aa637566e0ab
WebToLaure <lmarmus@hotmail.fr>
2023-05-09 10:47:29 +0200

    feat:topicCards

--- src/components/topicsCards/topicsCards.tsx
+++ src/components/topicsCards/topicsCards.tsx
@@ -2,35 +2,42 @@ import { useState, useEffect } from 'react';
 import './topicsCards.css';
 import TopicCard from '../topicCard/topicCard';
 import { NavLink, useParams } from 'react-router-dom';
-import SideBar from '../sidebar/sidebar';
 
-type TTop = {
+
+export type TTop = {
     id: number,
     continent: string,
     title: string,
     destinations: string,
     content: string,
-
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt: Date
 }
 
-/* type TImg = {
+type TImg = {
     id: number,
     originalName: string,
     file: string,
-   
-} */
+
+}
 
 
 export default function TopicsCards() {
 
-    const [topicsCards, setTopicsCards] = useState<TTop[] | null>(null)
-    const [continent, setContinent] = useState("")
-   /*  const [images, setImages] = useState<TImg[] | null>(null); *///a controler
 
+
+    const [topicsCards, setTopicsCards] = useState<TTop[] | null>(null);
+    const [continent, setContinent] = useState("");
+
+    
+    const [images, setImages] = useState<TImg[]>([]);
+    const [imageInput, setImageInput] = useState(0);
 
     let { id } = useParams();
     console.log('continent id:', id);
     useEffect(() => {
+
         console.log("useEffect est appelé");
 
         const getTopicsCards = async () => {
@@ -47,8 +54,10 @@ export default function TopicsCards() {
 
             setTopicsCards(responseJson.data.topics);
             setContinent(responseJson.data.continent);
-            /* setImages(responseJson.data.topics.images) *///a contrôler
-            console.log(/* setImages, */ "essai");
+            setImages(responseJson.data.topics.images)
+            console.log(continent, "essai");
+            console.log(topicsCards, "try");
+
 
         };
 
@@ -57,12 +66,22 @@ export default function TopicsCards() {
 
     }, [id])
 
+    /* function findImage(item: TImg) {
+        const index =images!.findIndex(elm => elm.id === item.id);
+        images![index] = item;
+        setImageInput({ ...images });
+
+    } */
+
 
     return (
         <>
+            <div className='d-flex flex-row justify-content-end'>
+                <NavLink to="/"><button className="retour mt-4 me-4 p-1 text-light">Back to Home</button></NavLink>
+            </div>
+
             <div className="h1 text-center mt-5 border-bottom border-top">{continent}</div>
             <div className="container d-flex align-items-center justify-content-center gap-3 px-0 mt-4">
-
                 {topicsCards?.map(item => <TopicCard item={item} key={item.id} />)}
             </div>
 
